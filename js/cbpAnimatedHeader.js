3d95d15223c75c97dbc985ba6d3664f705059a09
Justin Wilson <lengjaihk@gmail.com>
2016-07-05 18:09:19 -0400

    few updates
    Updated html, css, added/removed portfolio items, further optimized
    images, updated resume, etc.

--- js/cbpAnimatedHeader.js
+++ js/cbpAnimatedHeader.js
@@ -13,13 +13,13 @@ var cbpAnimatedHeader = (function() {
 	var docElem = document.documentElement,
 		header = document.querySelector( '.navbar-default' ),
 		didScroll = false,
-		changeHeaderOn = 90;
+		changeHeaderOn = 50;
 
 	function init() {
 		window.addEventListener( 'scroll', function( event ) {
 			if( !didScroll ) {
 				didScroll = true;
-				setTimeout( scrollPage, 90 );
+				setTimeout( scrollPage, 50 );
 			}
 		}, false );
 	}
